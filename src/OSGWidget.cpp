670d959ff48b53a44a1aeb93e666f40cd75bc5ff
pranjal <pranjal.swarup@gmail.com>
2016-02-03 17:19:23 +0800

    [Changes] Render a pyramid geometry from triangles
    [TODO]
    Render Obj mesh from triangle and normal data instead of adding it to root as a Node object

--- src/OSGWidget.cpp
+++ src/OSGWidget.cpp
@@ -234,17 +234,80 @@ void OSGWidget::setFile(QString fileName){
     if(!fileName.isEmpty()){
         osg::ref_ptr<osg::Node> model= osgDB::readNodeFile(fileName.toUtf8().constData());
         root->addChild( model.get() );
-
-        // Set material for basic lighting and enable depth tests.
-        osg::StateSet* stateSet = root.get()->getOrCreateStateSet();
-        osg::Material* material = new osg::Material;
-
-        material->setColorMode( osg::Material::AMBIENT_AND_DIFFUSE );
-
-        stateSet->setAttributeAndModes( material, osg::StateAttribute::ON );
-        stateSet->setMode( GL_DEPTH_TEST, osg::StateAttribute::ON );
-
     }
+    else
+    {
+        osg::Geode* pyramidGeode = new osg::Geode();
+        osg::Geometry* pyramidGeometry = new osg::Geometry();
+
+        pyramidGeode->addDrawable(pyramidGeometry);
+        root->addChild(pyramidGeode);
+
+        osg::Vec3Array* pyramidVertices = new osg::Vec3Array;
+        pyramidVertices->push_back( osg::Vec3( 0, 0, 0) ); // front left
+        pyramidVertices->push_back( osg::Vec3(10, 0, 0) ); // front right
+        pyramidVertices->push_back( osg::Vec3(10,10, 0) ); // back right
+        pyramidVertices->push_back( osg::Vec3( 0,10, 0) ); // back left
+        pyramidVertices->push_back( osg::Vec3( 5, 5,10) ); // peak
+
+        pyramidGeometry->setVertexArray( pyramidVertices );
+
+        osg::DrawElementsUInt* pyramidBase =
+                new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+        pyramidBase->push_back(3);
+        pyramidBase->push_back(2);
+        pyramidBase->push_back(1);
+        pyramidBase->push_back(0);
+        pyramidGeometry->addPrimitiveSet(pyramidBase);
+
+        osg::DrawElementsUInt* pyramidFaceOne =
+                new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+        pyramidFaceOne->push_back(0);
+        pyramidFaceOne->push_back(1);
+        pyramidFaceOne->push_back(4);
+        pyramidGeometry->addPrimitiveSet(pyramidFaceOne);
+
+        osg::DrawElementsUInt* pyramidFaceTwo =
+                new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+        pyramidFaceTwo->push_back(1);
+        pyramidFaceTwo->push_back(2);
+        pyramidFaceTwo->push_back(4);
+        pyramidGeometry->addPrimitiveSet(pyramidFaceTwo);
+
+        osg::DrawElementsUInt* pyramidFaceThree =
+                new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+        pyramidFaceThree->push_back(2);
+        pyramidFaceThree->push_back(3);
+        pyramidFaceThree->push_back(4);
+        pyramidGeometry->addPrimitiveSet(pyramidFaceThree);
+
+        osg::DrawElementsUInt* pyramidFaceFour =
+                new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+        pyramidFaceFour->push_back(3);
+        pyramidFaceFour->push_back(0);
+        pyramidFaceFour->push_back(4);
+        pyramidGeometry->addPrimitiveSet(pyramidFaceFour);
+
+        osg::Vec4Array* colors = new osg::Vec4Array;
+        colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f) ); //index 0 red
+        colors->push_back(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f) ); //index 1 green
+        colors->push_back(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f) ); //index 2 blue
+        colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f) ); //index 3 white
+        colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f) ); //index 4 red
+
+        pyramidGeometry->setColorArray(colors);
+        pyramidGeometry->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+    }
+
+
+    // Set material for basic lighting and enable depth tests.
+    osg::StateSet* stateSet = root.get()->getOrCreateStateSet();
+    osg::Material* material = new osg::Material;
+
+    material->setColorMode( osg::Material::AMBIENT_AND_DIFFUSE );
+
+    stateSet->setAttributeAndModes( material, osg::StateAttribute::ON );
+    stateSet->setMode( GL_DEPTH_TEST, osg::StateAttribute::ON );
 
     osg::Camera* camera = new osg::Camera;
     camera->setViewport( 0, 0, this->width(), this->height() );
