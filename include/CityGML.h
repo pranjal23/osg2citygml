8246c8e72d1bd54ade1fb6f4e4a7c6659774e2c7
pranjal <pranjal.swarup@gmail.com>
2016-07-21 18:39:20 +0800

    [Add] - Added GML schema dependencies - Some filename changes etc.

---
+++ include/CityGML.h
@@ -0,0 +1,10 @@
+#ifndef CITYGMLHELPER_H__
+#define CITYGMLHELPER_H__
+
+class LOD {
+    public :
+        LOD(void);
+        unsigned int LOD1 = 1;
+        unsigned int LOD2 = 2;
+};
+#endif
---
+++ include/SchemaValidator.h
@@ -0,0 +1,31 @@
+#ifndef SCHEMAVALIDATOR_H__
+#define SCHEMAVALIDATOR_H__
+
+#include <QtCore>
+#include <QtXmlPatterns>
+
+class SchemaValidator
+{
+public:
+    SchemaValidator(void)
+    {
+
+    }
+
+    void validate()
+    {
+        QFile file("schema/CityGML.xsd");
+        file.open(QIODevice::ReadOnly);
+
+
+        QXmlSchema schema;
+        schema.load(&file, QUrl::fromLocalFile(file.fileName()));
+
+        if (schema.isValid())
+            qDebug() << "CityGML schema is valid";
+        else
+            qDebug() << "CityGML schema is invalid";
+    }
+};
+
+#endif //SCHEMAVALIDATOR_H__
